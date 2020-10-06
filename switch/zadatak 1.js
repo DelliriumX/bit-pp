d6de36e8faea236b450fa8ee01cc77f8b40893ab
pera <classroom@bgit.rs>
2020-10-06 11:15:23 +0200

    switch zadatak 1

---
+++ switch/zadatak 1.js
@@ -0,0 +1,36 @@
+// primimo broj = input
+// obradimo input
+// ispisemo dan u nedelji (tekstualno) na osnovu inputa= output
+
+var input = 4
+
+switch (input) {
+    case 1: {
+        console.log("Ponedeljak");
+        break;
+    }
+    case 2: {
+        console.log("Utorak");
+        break;
+    }
+    case 3: {
+        console.log("Sreda");
+        break;
+    }
+    case 4: {
+        console.log("Cetvrtak");
+        break;
+    }
+    case 5: {
+        console.log("Petak");
+        break;
+    }
+    case 6: {
+        console.log("Subota");
+        break;
+    }
+    case 7: {
+        console.log("Nedelja");
+        break;
+    }
+}
