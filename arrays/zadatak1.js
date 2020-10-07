f9b0e7580b8c5dcb80955bb5c8be76c07bded628
pera <classroom@bgit.rs>
2020-10-07 11:33:20 +0200

    arrays zadatak 1

---
+++ arrays/zadatak1.js
@@ -0,0 +1,8 @@
+// 1. napisati array meseci u godini
+// 2. putem console.log-a napisati JUN, OKTOBAR, JANUAR
+
+var meseci = ['januar', 'februar', 'mart', 'april', 'maj', 'jun', 'jul', 'avgust', 'septembar', 'oktobar', 'novembar', 'decembar']
+
+console.log(meseci[5])
+console.log(meseci[9])
+console.log(meseci[0])
